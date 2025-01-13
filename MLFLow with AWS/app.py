eb15d984b04c6c5f5af10dd89853f531a8cdb23f
Karush2807 <arushkarnatak1881@gmail.com>
2025-01-13 23:47:30 +0530

    code completed

--- MLFLow with AWS/app.py
+++ MLFLow with AWS/app.py
@@ -23,3 +23,50 @@ def eval_metrics(actual, pred):
 
 
 if __name__=="__main__":
+
+    ## data ingestion and readin dataset
+    csv_url=(
+        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
+    )
+
+
+    try: 
+        data=pd.read_csv(csv_url, sep=';')
+    except Exception as e:
+        logger.exception("unable to fetch data")
+
+    ## train, test, split
+    train, test=train_test_split(data)
+
+    train_x=train.drop(['quality'], axis=1)
+    test_x=test.drop(["quality"], axis=1)
+    train_y=train[['quality']]
+    test_y=train[['quality']]
+
+    ## will be using elasticNet from sklearn
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr=ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        preeicted_qualities=lr.predict(test_x)
+        (rmse, mae, r2)= eval_metrics(test_y, preeicted_qualities)
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+    ## for the remote server we need to the setup
+    remote_server_uri=" "
+    mlflow.set_tracking_uri(remote_server_uri) # ye hmara aws wala remote server hoga
+
+    tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+
+    if tracking_url_type_store!="file":
+        mlflow.sklearn.log_model(
+            lr, "model", registered_model_name="ElasticNetWineModel"
+    )
+    else:
+        mlflow.sklearn.load_model(lr, "model")
